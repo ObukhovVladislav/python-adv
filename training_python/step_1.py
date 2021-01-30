b8704d9ac0fe74a0e9c9ebef6b94a0c37be3c11b
Vladislav Obv <VladislavObv@example.com>
2021-01-30 19:16:36 +0500

    Training new material

---
+++ training_python/step_1.py
@@ -0,0 +1,7 @@
+name = 'иван'
+# заглавная первая буква, а-я,
+# dict
+alphabet = {chr(el) for el in range(ord('а'), ord('я') + 1)}
+# print(type(alphabet))
+# print('d' in alphabet)
+print(alphabet)
