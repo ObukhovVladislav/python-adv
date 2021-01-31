8dd31d4a9ee7bba8c845d5e834f41b20d8b4b504
Vladislav Obv <VladislavObv@example.com>
2021-01-31 10:19:04 +0500

    Training new material

--- training_python/step_2.py
+++ training_python/step_2.py
@@ -2,8 +2,21 @@ name = 'иван'
 # заглавная первая буква, а-я,
 # dict
 alphabet = {chr(el) for el in range(ord('а'), ord('я') + 1)}
+alphabet.add('ё')
 
-set_1 = {'а', 'б', 'в'}
-set_2 = {'а', 'б'}
+# set_1 = {'а', 'б', 'в'}
+# set_2 = {'а', 'б'}
+#
+# print(set_1)
+# print(set_2 - set_1)
+# print(set_1 - set_2)
 
-print(set_2 - set_1)
+is_valid = not set(name) - alphabet
+print(is_valid)
+
+is_valid = True
+for letter in name:
+    if letter not in alphabet:
+        is_valid = False
+        break
+print(is_valid)
