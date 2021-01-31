341dc5952cf4d88dbdc04c389c8081c33dfb29d9
Vladislav Obv <VladislavObv@example.com>
2021-01-31 09:42:41 +0500

    Training new material

--- training_python/step_1.py
+++ training_python/step_1.py
@@ -2,6 +2,15 @@ name = 'иван'
 # заглавная первая буква, а-я,
 # dict
 alphabet = {chr(el) for el in range(ord('а'), ord('я') + 1)}
-# print(type(alphabet))
+alphabet_2 = [chr(el) for el in range(ord('а'), ord('я') + 1)]
+alphabet_3 = {chr(el): el for el in range(ord('а'), ord('я') + 1)}
+
+print(type(alphabet), type(alphabet_2), type(alphabet_3))
+
 # print('d' in alphabet)
-print(alphabet)
+# print('d' in alphabet_2)
+# print(alphabet)
+# print(alphabet_3)
+print('z' in alphabet, 'г' in alphabet)
+print('z' in alphabet_2, 'г' in alphabet_2)
+print('z' in alphabet_3, 'г' in alphabet_3)
