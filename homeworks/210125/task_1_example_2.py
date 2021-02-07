c3ff0019b1e3f00605ccb19f1c136dd005d50866
Vladislav Obv <VladislavObv@example.com>
2021-02-07 20:42:18 +0500

    Regular requests

---
+++ homeworks/210125/task_1_example_2.py
@@ -0,0 +1,17 @@
+# a = range(5)
+# list(a)
+#
+
+
+# def infinite_sequence():
+#     num = 0
+#     while True:
+#         yield num
+#         num += 1
+
+# for i in infinite_sequence():
+#     print(i, end=" ")
+
+# gen = infinite_sequence()
+# next(gen)
+
