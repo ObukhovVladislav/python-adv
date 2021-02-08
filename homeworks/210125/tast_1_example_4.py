1a21c4c0c56616f7412b826f8f942ebed2f4c1b6
Vladislav Obv <VladislavObv@example.com>
2021-02-08 10:03:35 +0500

    Regular requests

--- homeworks/210125/tast_1_example_4.py
+++ homeworks/210125/tast_1_example_4.py
@@ -5,7 +5,7 @@
 #     reversed_num = 0
 #
 #     while temp != 0:
-#         reversed_num = (reversed_num * 10) + (temp % 10)
+#         reversed_num = reversed_num * 10 + temp % 10
 #         temp = temp // 10
 #
 #     if num == reversed_num:
