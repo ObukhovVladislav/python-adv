eab83e479a0ef83ed53fc106450c8e0c53af5497
Vladislav Obv <VladislavObv@example.com>
2021-02-21 21:49:21 +0500

    hw_1_simple_decorators

--- homeworks/210215/hw_1/simple_decorators.py
+++ homeworks/210215/hw_1/simple_decorators.py
@@ -1,8 +1,32 @@
-def my_decorator(func):
+# def my_decorator(func):
+#     def wrapper():
+#         print("Something is happening before the function is called.")
+#         func()
+#         print("Something is happening after the function is called.")
+#
+#     return wrapper
+#
+#
+# def say_whee():
+#     print("Whee!")
+#
+#
+# say_whee = my_decorator(say_whee)
+#
+# say_whee()
+# say_whee = my_decorator(say_whee)
+# # print(say_whee)
+
+
+from datetime import datetime
+
+
+def not_during_the_night(func):
     def wrapper():
-        print("Something is happening before the function is called.")
-        func()
-        print("Something is happening after the function is called.")
+        if 7 <= datetime.now().hour < 22:
+            func()
+        else:
+            pass  # Hush, the neighbors are asleep
 
     return wrapper
 
@@ -11,6 +35,5 @@ def say_whee():
     print("Whee!")
 
 
-say_whee = my_decorator(say_whee)
-
-say_whee()
+say_whee = not_during_the_night(say_whee)
+# say_whee()
