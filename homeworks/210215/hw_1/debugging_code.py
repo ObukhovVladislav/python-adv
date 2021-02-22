191154d8774ee4816855d2c047eda7129f07375f
Vladislav Obv <VladislavObv@example.com>
2021-02-22 07:48:18 +0500

    hw_1_debugging_code

--- homeworks/210215/hw_1/debugging_code.py
+++ homeworks/210215/hw_1/debugging_code.py
@@ -1,28 +1,44 @@
-import functools
+# import functools
+#
+#
+# def debug(func):
+#     """Распечатайте подпись функции и возвращаемое значение"""
+#
+#     @functools.wraps(func)
+#     def wrapper_debug(*args, **kwargs):
+#         args_repr = [repr(a) for a in args]  # 1
+#         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+#         signature = ", ".join(args_repr + kwargs_repr)  # 3
+#         print(f"Вызов {func.__name__}({signature})")
+#         value = func(*args, **kwargs)
+#         print(f"{func.__name__!r} вернулся {value!r}")  # 4
+#         return value
+#
+#     return wrapper_debug
+#
+#
+# @debug
+# def make_greeting(name, age=None):
+#     if age is None:
+#         return f"Привет {name}!"
+#     else:
+#         return f"Привет {name}! {age} вы уже растете!"
+#
+#
+# make_greeting("Владислав")
+# make_greeting("Владислав", age=18)
+# make_greeting(name="Евгений", age=18)
 
 
-def debug(func):
-    """Распечатайте подпись функции и возвращаемое значение"""
-
-    @functools.wraps(func)
-    def wrapper_debug(*args, **kwargs):
-        args_repr = [repr(a) for a in args]  # 1
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
-        signature = ", ".join(args_repr + kwargs_repr)  # 3
-        print(f"Вызов {func.__name__}({signature})")
-        value = func(*args, **kwargs)
-        print(f"{func.__name__!r} вернулся {value!r}")  # 4
-        return value
-
-    return wrapper_debug
-
-
-@debug
-def make_greeting(name, age=None):
-    if age is None:
-        return f"Привет {name}!"
-    else:
-        return f"Привет {name}! {age} вы уже растете!"
-
-
-make_greeting("Владислав")
+# import math
+# from decorators import debug
+#
+# # Apply a decorator to a standard library function
+# math.factorial = debug(math.factorial)
+#
+#
+# def approximate_e(terms=18):
+#     return sum(1 / math.factorial(n) for n in range(terms))
+#
+#
+# approximate_e(5)
