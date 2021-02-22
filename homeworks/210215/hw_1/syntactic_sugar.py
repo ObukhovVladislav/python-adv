ea0e586b94cec3276b083ebb81a0c0628048147a
Vladislav Obv <VladislavObv@example.com>
2021-02-22 07:36:44 +0500

    refactoring

--- homeworks/210215/hw_1/syntactic_sugar.py
+++ homeworks/210215/hw_1/syntactic_sugar.py
@@ -1,8 +1,8 @@
 def my_decorator(func):
     def wrapper():
-        print("Something is happening before the function is called.")
+        print("Что-то происходит до вызова функции.")
         func()
-        print("Something is happening after the function is called.")
+        print("Что-то происходит после вызова функции.")
 
     return wrapper
 
