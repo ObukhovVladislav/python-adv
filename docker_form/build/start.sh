cc8d674c37202727a770a81149860b1e93daaefb
ObukhovVladislav <vobukhov10@gmail.com>
2021-03-14 12:19:18 +0500

    docker_form

--- docker_form/build/start.sh
+++ docker_form/build/start.sh
@@ -2,5 +2,9 @@
 
 echo "Building kpk docker"
 docker build -f ./build/docker/Dockerfile -t kpk ./build/docker/
-echo "Starting kpk docker"
 
+echo "Starting kpk docker"
+docker run --rm -it \
+    -v $(pwd)/sources:/home/kpk/bin \
+    -v $(pwd)/data:/home/kpk/data \
+    kpk
