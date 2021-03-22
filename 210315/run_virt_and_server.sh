ba21edbcff4a7a288832740e72b154e6f52374c4
ObukhovVladislav <vobukhov10@gmail.com>
2021-03-22 09:37:28 +0500

    homework

--- 210315/run_virt_and_server.sh
+++ 210315/run_virt_and_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Starting kpk server"
-source /home/user/virtual/bin/activate && \
+source /home/vladislav/virtual/bin/activate && \
 python "sources/my_messenger/manage.py" runserver 0.0.0.0:8011
