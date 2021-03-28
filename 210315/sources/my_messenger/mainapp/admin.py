4534123a813634404824ffd2721122568daee1d2
ObukhovVladislav <vobukhov10@gmail.com>
2021-03-28 18:50:28 +0500

    homework

--- 210315/sources/my_messenger/mainapp/admin.py
+++ 210315/sources/my_messenger/mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from mainapp.models import Dialog, DialogMembers, Message
+
+admin.site.register(Dialog)
+admin.site.register(DialogMembers)
+admin.site.register(Message)
