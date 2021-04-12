f3980a67409c3185a485ca26d523577a9f21844c
ObukhovVladislav <vobukhov10@gmail.com>
2021-04-12 09:45:01 +0500

    refactoring

--- 210315/sources/my_messenger/mainapp/urls.py
+++ 210315/sources/my_messenger/mainapp/urls.py
@@ -5,6 +5,6 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.index, name='index'),
-    path('dialog/<int:dialog_pk>', mainapp.dialog, name='dialog'),
+    path('dialog/<int:dialog_pk>/', mainapp.dialog, name='dialog'),
     path('dialogue_creation/', mainapp.dialogue_creation, name='dialogue_creation'),
 ]
