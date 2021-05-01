5d435c38a2d690e3bd1c588d5e50f94370e9fcef
ObukhovVladislav <vobukhov10@gmail.com>
2021-05-01 11:08:53 +0500

    main.js

---
+++ 210315/sources/my_messenger/static/js/main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    console.log('ready');
+    $('.dialog-messages').on('click', 'dialog-update', function (e) {
+        e.preventDefault();
+        $.ajax( {
+            url: e.target.href,
+            success: function (){
+                $('.card').update();
+            }
+        })
+    })
+}
