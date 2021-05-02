0d0de3f6710b352054d007d9e7202c8e1e42f508
ObukhovVladislav <vobukhov10@gmail.com>
2021-05-02 10:32:00 +0500

    update messages

--- 210315/sources/my_messenger/static/js/main.js
+++ 210315/sources/my_messenger/static/js/main.js
@@ -1,11 +1,33 @@
+let $dialogDOMMessages;
+
+function messageRender(message) {
+    let messageDom = $('.message-' + message.pk);
+    let textMessage;
+    if (!messageDom.length) {
+        let messageNew = document.createElement('li');
+        messageNew.classList.add('message-' + message.pk);
+        textMessage = message.username + " (" + message.created + ") - " + message.text;
+        messageNew.innerHTML = textMessage;
+        let parent = $dialogDOMMessages.find('.messages-list');
+        parent.prepend(messageNew);
+    }
+}
+
+
 window.onload = function () {
     console.log('ready');
-    $('.dialog-messages').on('click', 'dialog-update', function (e) {
+    $dialogDOMMessages = $('.dialog-messages');
+    $dialogDOMMessages.on('click', 'a.dialog-update', function (e) {
         e.preventDefault();
-        $.ajax( {
+        $.ajax({
             url: e.target.href,
-            success: function (response){
-                console.log(response);
+            success: function (response) {
+                let messages_new = response.messages_new
+                if (messages_new) {
+                    messages_new.forEach(function (el, idx) {
+                        messageRender(el);
+                    })
+                }
             }
         })
     })
